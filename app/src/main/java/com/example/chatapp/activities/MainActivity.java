5a7d707aab940c07c81d922b86ed43cffce3e9c2
mericalp <83503845+mericalp@users.noreply.github.com>
2023-02-11 16:43:53 +0300

    FCMToken

--- app/src/main/java/com/example/chatapp/activities/MainActivity.java
+++ app/src/main/java/com/example/chatapp/activities/MainActivity.java
@@ -2,14 +2,72 @@ package com.example.chatapp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.chatapp.R;
+import com.example.chatapp.databinding.ActivityMainBinding;
+import com.example.chatapp.utilities.Constants;
+import com.example.chatapp.utilities.PreferenceManager;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.messaging.FirebaseMessaging;
+
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    private PreferenceManager preferenceManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        preferenceManager = new PreferenceManager(getApplicationContext());
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setListeners();
+        getToken();
     }
+    private void setListeners(){
+        binding.imageSignOut.setOnClickListener(view -> signOut());
+    }
+
+    // Load User Details...
+    //
+
+
+
+    private void showToast(String message){
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+    private void getToken(){
+        FirebaseMessaging.getInstance().getToken().addOnSuccessListener(this::updateToken);
+    }
+    private void updateToken(String token){
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        DocumentReference documentReference = database.collection(Constants.KEY_COLLECTION_USERS)
+                .document(preferenceManager.getString(Constants.KEY_USER_ID));
+        documentReference.update(Constants.KEY_FCM_TOKEN, token)
+                .addOnFailureListener(e -> showToast("unable to update token"));
+    }
+    private void signOut(){
+        showToast("Singing out....");
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        DocumentReference documentReference = database.collection(Constants.KEY_COLLECTION_USERS)
+                .document(preferenceManager.getString(Constants.KEY_USER_ID));
+        HashMap<String,Object> updates = new HashMap<>();
+        updates.put(Constants.KEY_FCM_TOKEN, FieldValue.delete());
+        documentReference.update(updates)
+                .addOnSuccessListener(unused -> {
+                   preferenceManager.clear();
+                   startActivity(new Intent(getApplicationContext(),SignInActivity.class));
+                   finish();
+                })
+                .addOnFailureListener(e -> showToast("Unable to sign out"));
+    }
+
 }
